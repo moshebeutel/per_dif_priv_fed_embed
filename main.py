e61fcaee211079c5d1b12c0042770948a5189861
Moshe Beutel <moshebeutel@gmail.com>
2023-02-12 17:06:34 +0200

    conv net with centroids

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import argparse
-from datetime import time
+import time
 import torch.cuda
 import wandb
 from common.config import Config
 from dataset.create_data_loaders import get_data_loaders
-# from differential_privacy.get_private_model import get_private_model
+from differential_privacy.get_private_model import get_private_model
+from models.conv_net_with_centroids import ConvNetWithCentroids
 from models.mlp_net import MlpNet
 from models.conv_net import ConvNet
 from train.utils import get_loss_and_opt, save_model, train_method
 
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 
 def single_train(batch_size, learning_rate, epochs=50, save_final_model=False):
     trainloader, testloader = get_data_loaders(dataset_name=Config.DATASET, batch_size=batch_size)
     print('train loader', len(trainloader))
     # net = MlpNet()  # ConvNet().to(device)
-    net = ConvNet()
+    net = ConvNetWithCentroids()
     net.to(device)
     criterion, optimizer = get_loss_and_opt(net, learning_rate=learning_rate)
 
@@ -26,7 +27,7 @@ def single_train(batch_size, learning_rate, epochs=50, save_final_model=False):
     priv_model, priv_optimizer, priv_loader = net, optimizer, trainloader
 
     train_method(trainloader=priv_loader, testloader=testloader, net=priv_model, criterion=criterion,
-                 optimizer=priv_optimizer, epochs=epochs,
+                 optimizer=priv_optimizer, epochs=epochs, device=device,
                  save_model_every=100)
     if save_final_model:
         filename_prefix = \
@@ -40,8 +41,8 @@ def run_single_train():
     GRAD_OR_SIGN = 'sgd'
     assert GRAD_OR_SIGN in ['sgd', 'sign', 'grad', 'sgd_dp', 'sgd_dp_sign', 'sgd_sign_dp']
 
-    BATCH_SIZE = 64
-    EPOCHS = 50
+    BATCH_SIZE = 256
+    EPOCHS = 200
     LEARNING_RATE = 0.01
 
     wandb.init(project="emg_gp_moshe", entity="emg_diff_priv",
@@ -49,7 +50,6 @@ def run_single_train():
     wandb.config.update({'batch_size': BATCH_SIZE, 'learning_rate': LEARNING_RATE})
 
     single_train(batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, epochs=EPOCHS, save_final_model=True)
-
 
 
 def run_sweep():
