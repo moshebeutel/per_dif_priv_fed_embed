2baacd3e67d1ae546312726e108ed402c9f752cb
moshe <moshebeutel@gmail.com>
2023-02-14 11:13:39 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Package configuration."""
+from setuptools import find_packages, setup
+
+setup(
+    name="per_dif_priv_fed_embed",
+    version="0.0.1",
+    packages=find_packages(where="per_dif_priv_fed_embed"),
+    package_dir={"": "./"},
+)
